ae8ad0536c77197695cd1ab53b5db63e1101976b
Wassim <wasshimu.dev@gmail.com>
2024-11-12 11:35:42 +0100

    Add display none on navbar-mobile and var icone and modal

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('connecté !')
+
+// Je selectionne et stocke les elements
+// Icone burger
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+// DIV modal
+const modal = document.querySelector('.modal')
+console.log(modal)
+
+
+
+
